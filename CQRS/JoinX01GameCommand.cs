8d26f71e4ed6ecd4ddc9871d846320b8c766704a
Mike Pattyn <mike.pattyn1@gmail.com>
2023-05-17 19:54:17 +0200

    Initial commit - NORUN

---
+++ CQRS/JoinX01GameCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Amazon.Lambda.APIGatewayEvents;
+using MediatR;
+
+public class JoinX01GameCommand : IRequest<APIGatewayProxyResponse>
+{
+    public long GameId { get; set; }
+    public Guid PlayerId { get; set; }
+    public string PlayerName { get; set; }
+}
---
+++ CQRS/JoinX01GameCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Threading;
+using System.Threading.Tasks;
+using Flyingdarts.Persistence;
+using MediatR;
+using Amazon.DynamoDBv2.DataModel;
+using Flyingdarts.Lambdas.Shared;
+using Flyingdarts.Shared;
+using Microsoft.Extensions.Options;
+using System;
+
+public class JoinX01GameCommandHandler : IRequestHandler<JoinX01GameCommand, APIGatewayProxyResponse>
+{
+    private readonly IDynamoDBContext _dbContext;
+    private readonly ApplicationOptions _applicationOptions;
+    public JoinX01GameCommandHandler(IDynamoDBContext dbContext, IOptions<ApplicationOptions> applicationOptions)
+    {
+        _dbContext = dbContext;
+        _applicationOptions = applicationOptions.Value;
+    }
+    public async Task<APIGatewayProxyResponse> Handle(JoinX01GameCommand request, CancellationToken cancellationToken)
+    {
+        await JoinGame(request.GameId, request.PlayerId, cancellationToken);
+
+        var socketMessage = new SocketMessage<JoinX01GameCommand>
+        {
+            Action = "v2/games/x01/join",
+            Message = request
+        };
+        return new APIGatewayProxyResponse { StatusCode = 200, Body = JsonSerializer.Serialize(socketMessage) };
+    }
+
+    private async Task JoinGame(long gameId, Guid playerId, CancellationToken cancellationToken)
+    {
+        var gamePlayer = GamePlayer.Create(gameId, playerId);
+        var gamePlayerWrite = _dbContext.CreateBatchWrite<GamePlayer>(_applicationOptions.ToOperationConfig()); gamePlayerWrite.AddPutItem(gamePlayer);
+
+        await gamePlayerWrite.ExecuteAsync(cancellationToken);
+    }
+}
