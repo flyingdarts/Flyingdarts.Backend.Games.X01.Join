d82c35346d58380e336750dff6187d5ea2b58bb3
daniela <whateverellie@gmail.com>
2023-06-01 22:37:14 +0200

    Added a post processor to update game status when there are 2 players

---
+++ CQRS/JoinX01GameCommandGameStatusUpdater.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Flyingdarts.Persistence;
+using Flyingdarts.Shared;
+using MediatR.Pipeline;
+using Microsoft.Extensions.Options;
+
+namespace Flyingdarts.Backend.Games.X01.Join.CQRS
+{
+    public class JoinX01GameCommandGameStatusUpdater : IRequestPostProcessor<JoinX01GameCommand, APIGatewayProxyResponse>
+    {
+        private readonly IDynamoDBContext _dbContext;
+        private readonly ApplicationOptions _applicationOptions;
+
+        public JoinX01GameCommandGameStatusUpdater(IDynamoDBContext dbContext, IOptions<ApplicationOptions> applicationOptions)
+        {
+            _dbContext = dbContext;
+            _applicationOptions = applicationOptions.Value;
+        }
+
+        public async Task Process(JoinX01GameCommand request, APIGatewayProxyResponse response, CancellationToken cancellationToken)
+        {
+            if (request.Players.Count() == 2) {
+                request.Game.Status = GameStatus.Started;
+            }
+
+            var gameWrite = _dbContext.CreateBatchWrite<Game>(_applicationOptions.ToOperationConfig());
+
+            gameWrite.AddPutItem(request.Game);
+
+            await gameWrite.ExecuteAsync(cancellationToken);
+        }
+    }
+}
