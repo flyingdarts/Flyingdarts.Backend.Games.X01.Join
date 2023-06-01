d3fb041e8df1eb0a50b5efd191c37b6278b84967
daniela <whateverellie@gmail.com>
2023-06-01 14:57:04 +0200

    Return history if there is any

--- CQRS/JoinX01GameCommand.cs
+++ CQRS/JoinX01GameCommand.cs
@@ -8,7 +8,16 @@ public class JoinX01GameCommand : IRequest<APIGatewayProxyResponse>
     public string GameId { get; set; }
     public string PlayerId { get; set; }
     public string PlayerName { get; set; }
-    public Game? Game { get; set; }
-    internal string ConnectionId { get; set; }
+    public Game Game { get; set; }
+    internal List<GamePlayer> Players { get; set; }
+    internal List<GameDart> Darts { get; set; }
+    internal List<User> Users { get; set; }    internal string ConnectionId { get; set; }
     public Dictionary<string,object>? Metadata {get;set;}
+    public Dictionary<string, ScoreboardRecord> History { get; set; }
+}
+
+public class ScoreboardRecord
+{
+    public int Score { get; set; }
+    public List<int> History { get; set; }
 }
