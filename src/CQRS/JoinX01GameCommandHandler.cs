1f73b0ad81ba3fd3ea119683022c1fbdc81c4f4f
Mike Pattyn <mike.pattyn1@gmail.com>
2023-10-06 11:11:57 +0200

    Update

--- src/CQRS/JoinX01GameCommandHandler.cs
+++ src/CQRS/JoinX01GameCommandHandler.cs
@@ -13,13 +13,6 @@ public record JoinX01GameCommandHandler(IDynamoDbService DynamoDbService) : IReq
 {
     public async Task<APIGatewayProxyResponse> Handle(JoinX01GameCommand request, CancellationToken cancellationToken)
     {
-        //request.History = new();
-        //request.Players.ForEach(p =>
-        //{
-        //    request.History.Add(p.PlayerId, new());
-        //    request.History[p.PlayerId].History = request.Darts.OrderBy(x => x.CreatedAt).Where(x => x.PlayerId == p.PlayerId).Select(x => x.Score).ToList();
-        //});
-
         var socketMessage = new SocketMessage<JoinX01GameCommand>
         {
             Action = "v2/games/x01/join",
@@ -27,24 +20,11 @@ public record JoinX01GameCommandHandler(IDynamoDbService DynamoDbService) : IReq
         };
 
         request.Game = await DynamoDbService.ReadGameAsync(long.Parse(request.GameId), cancellationToken);
-        if (request.Game == null)
-        {
-            throw new Exception($"Game is null ${request.GameId}");
-        }
-
         request.Players = await DynamoDbService.ReadGamePlayersAsync(long.Parse(request.GameId), cancellationToken);
-        if (request.Players == null)
-        {
-            throw new Exception($"Game players is null ${request.GameId}");
-        }
-
         request.Users = await DynamoDbService.ReadUsersAsync(request.Players.Select(x => x.PlayerId).ToArray(), cancellationToken);
-        if (request.Users == null)
-        {
-            throw new Exception($"Users is null ${request.GameId}");
-        }
-
         request.Darts = await DynamoDbService.ReadGameDartsAsync(long.Parse(request.GameId), cancellationToken);
+
+        Metadata data = new Metadata();
 
         if (request.Game is not null)
         {
@@ -52,26 +32,62 @@ public record JoinX01GameCommandHandler(IDynamoDbService DynamoDbService) : IReq
 
             await DynamoDbService.WriteGamePlayerAsync(player, cancellationToken);
 
-            socketMessage.Message.Game = request.Game;
+           data.Game = request.Game;
         }
 
         if (request.Players is not null)
         {
-            var retVal = request.Players.Select(x =>
+            var orderedPlayers = request.Players.Select(x =>
             {
-                return new JoinX01GameCommand
+                return new PlayerDto
                 {
                     PlayerId = x.PlayerId,
                     PlayerName = request.Users.Single(y => y.UserId == x.PlayerId).Profile.UserName
                 };
-            }).ToArray();
+            }).OrderBy(x=>x.CreatedAt);
 
-            socketMessage.Message.Metadata = new Dictionary<string, object>
-            {
-                { "CurrentPlayers", retVal }
-            };
+            data.Players = orderedPlayers;
         }
+
+        if (request.Darts is not null)
+        {
+            data.Darts = new();
+            request.Players.ForEach(p =>
+            {
+                data.Darts.Add(p.PlayerId, new());
+                data.Darts[p.PlayerId].History = request.Darts.OrderBy(x => x.CreatedAt).Where(x => x.PlayerId == p.PlayerId).Select(x => x.Score).ToList();
+            });
+        }
+
+        socketMessage.Metadata = data.toDictionary();
 
         return new APIGatewayProxyResponse { StatusCode = 200, Body = JsonSerializer.Serialize(socketMessage) };
     }
+}
+
+
+class Metadata
+{
+    public Game Game { get; set; }
+    public IOrderedEnumerable<PlayerDto> Players { get; set; }
+    public Dictionary<string, ScoreboardRecord> Darts { get; set; }
+
+    public Dictionary<string, object> toDictionary()
+    {
+        var result = new Dictionary<string, object>
+        {
+            { "Game", Game },
+            { "Players", Players },
+            { "Darts", Darts }
+        };
+
+        return result;
+    }
+}
+
+class PlayerDto
+{
+    public String PlayerId { get; set; }
+    public String PlayerName { get; set; }
+    public DateTime CreatedAt { get; set; }
 }
